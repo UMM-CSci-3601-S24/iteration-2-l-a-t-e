b2b4f68bf000cd5984a0585c7511f5c82b3a6b1c
Ken Broden <brod0268@morris.umn.edu>
2024-02-24 16:54:44 -0600

    Hunt.java file created on server

---
+++ server/src/main/java/umm3601/hunt/Hunt.java
@@ -0,0 +1,35 @@
+package umm3601.hunt;
+
+import java.util.ArrayList;
+
+import org.mongojack.Id;
+import org.mongojack.ObjectId;
+
+@SuppressWarnings({"VisibilityModifier"})
+public class Hunt {
+
+  @ObjectId @Id
+
+  @SuppressWarnings({"MemberName"})
+  public String _id;
+
+  public String hostid;
+  public String title;
+  public String description;
+  public ArrayList<String> tasks;
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Hunt)) {
+      return false;
+    }
+    Hunt other = (Hunt) obj;
+    return _id.equals(other._id);
+  }
+
+  @Override
+  public int hashCode() {
+    return _id.hashCode();
+
+  }
+}
