866d2b0c3d9ac57a8fc445b4fd5127884beb4e4b
Ken Broden <brod0268@morris.umn.edu>
2024-02-25 15:14:33 -0600

    added a 'My Hunts' icon for /hunts
    test for hunt.service.ts is not passing

--- client/src/app/hunts/hunt.service.ts
+++ client/src/app/hunts/hunt.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, map } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { Hunt } from './hunt';
 
 
 @Injectable({
@@ -17,23 +17,24 @@ export class HuntService {
   constructor(private httpClient: HttpClient) {
   }
 
-/**
-*  @param filters a map that allows us to specify a target hostid
-*  @returns an `Observable` of an array of `Hunts`. Wrapping the array
-*   in an `Observable` means that other bits of of code can `subscribe` to
-*   the result (the `Observable`) and get the results that come back
-*   from the server after a possibly substantial delay (because we're
-*   contacting a remote server over the Internet).
-*/
-getHunts(filters?: { hostid?: string }): Observable<Hunt[]> {
-  let httpParams: HttpParams = new HttpParams();
-  if (filters) {
-    if (filters.hostid) {
-      httpParams = httpParams.set(this.hostKey, filters.hostid);
+  /**
+  *  @param filters a map that allows us to specify a target hostid
+  *  @returns an `Observable` of an array of `Hunts`. Wrapping the array
+  *   in an `Observable` means that other bits of of code can `subscribe` to
+  *   the result (the `Observable`) and get the results that come back
+  *   from the server after a possibly substantial delay (because we're
+  *   contacting a remote server over the Internet).
+  */
+  getHunts(filters?: { hostid?: string }): Observable<Hunt[]> {
+    let httpParams: HttpParams = new HttpParams();
+    if (filters) {
+      if (filters.hostid) {
+        httpParams = httpParams.set(this.hostKey, filters.hostid);
+      }
     }
-  }
 
-  return this.httpClient.get<Hunt[]>(this.huntUrl, {
-    params: httpParams
-  });
+    return this.httpClient.get<Hunt[]>(this.huntUrl, {
+      params: httpParams
+    });
+  }
 }
