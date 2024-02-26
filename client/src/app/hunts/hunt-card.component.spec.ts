dd979b1a2e7997a1b0c2264beb82f377483f05b1
Ken Broden <brod0268@morris.umn.edu>
2024-02-26 17:51:48 -0600

    Defined the hunt-card components
    Still not working

--- client/src/app/hunts/hunt-card.component.spec.ts
+++ client/src/app/hunts/hunt-card.component.spec.ts
@@ -1,19 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HuntCardComponent } from './hunt-card.component';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('HuntCardComponent', () => {
   let component: HuntCardComponent;
   let fixture: ComponentFixture<HuntCardComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HuntCardComponent]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HuntCardComponent,
+        MatCardModule,
+        BrowserAnimationsModule
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(HuntCardComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HuntCardComponent);
+    component = fixture.componentInstance;
+    component.hunt = {
+      _id: 'hunt1_id',
+      hostid: 'chris',
+      title: 'Chris\'s Hunt',
+      description: 'Chris\'s test hunt'
+    };
     fixture.detectChanges();
   });
 
--- client/src/app/hunts/hunt-card.component.ts
+++ client/src/app/hunts/hunt-card.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Hunt } from './hunt';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-hunt-card',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatListModule, MatIconModule, RouterLink],
   templateUrl: './hunt-card.component.html',
   styleUrl: './hunt-card.component.scss'
 })
