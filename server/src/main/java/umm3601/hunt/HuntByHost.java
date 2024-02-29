c90d39878d22f19fe7660b8b35712e9ce3562f49
Ken Broden <brod0268@morris.umn.edu>
2024-02-28 22:41:29 -0600

    Added HuntByHost and HuntIdTitle
    Concerned on how to proceed on the client side.  Need to ask Nic or Jaydon for thoughts.

---
+++ server/src/main/java/umm3601/hunt/HuntByHost.java
@@ -0,0 +1,14 @@
+package umm3601.hunt;
+
+import java.util.List;
+
+@SuppressWarnings({ "VisibilityModifier" })
+public class HuntByHost {
+
+  @SuppressWarnings({ "MemberName" })
+  public String _id;
+  public int count;
+  public List<HuntIdTitle> hunts;
+
+
+}
---
+++ server/src/main/java/umm3601/hunt/HuntIdTitle.java
@@ -0,0 +1,8 @@
+package umm3601.hunt;
+
+@SuppressWarnings("checkstyle:visibilitymodifier")
+public class HuntIdTitle {
+  @SuppressWarnings("checkstyle:membername")
+  public String _id;
+  public String title;
+}
