3f92bf1ab426147dc5fe44c525e9b876b93189d8
Ken <brod0268@morris.umn.edu>
2024-03-05 01:07:55 -0600

    Navigation to edit page works

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { HuntListComponent } from './hunts/hunt-list.component';
 import { HuntProfileComponent } from './hunts/hunt-profile.component';
 import { TaskListComponent } from './hunts/task-list.component';
+import { HuntEditComponent } from './hunts/edit.component';
 
 // Note that the 'hunts/new' route needs to come before 'hunts/:id'.
 // If 'hunts/:id' came first, it would accidentally catch requests to
@@ -12,9 +13,11 @@ import { TaskListComponent } from './hunts/task-list.component';
 const routes: Routes = [
   {path: '', component: HomeComponent, title: 'Home'},
   {path: 'hunts', component: HuntListComponent, title: 'My Hunts'},
+  {path: 'hunts/edit/:id', component: HuntEditComponent, title: 'Edit Hunt'},
   {path: 'hunts/:id', component: HuntProfileComponent, title: 'Hunt Profile'},
   {path: 'tasks', component: TaskListComponent, title: 'Task List'},
   {path: 'tasks/:id', component: TaskListComponent, title: 'Single Task'}
+
 ];
 
 @NgModule({
--- client/src/app/hunts/edit.component.ts
+++ client/src/app/hunts/edit.component.ts
@@ -1,37 +1,91 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HuntService } from '../services/hunt.service';
-import { TaskService } from '../services/task.service';
+import { HuntService } from './hunt.service';
+import { TaskService } from './task.service';
+import { Hunt } from './hunt';
+import { Task } from './task';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormBuilder, Validators, FormArray, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-hunt-edit',
-  templateUrl: './hunt-edit.component.html',
-  styleUrls: ['./hunt-edit.component.css']
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.scss'],
+  standalone: true,
+  imports: [ReactiveFormsModule,
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatIconModule]
 })
 export class HuntEditComponent implements OnInit {
   hunt: Hunt;
-  tasks: Task[];
 
   constructor(
     private route: ActivatedRoute,
     private huntService: HuntService,
-    private taskService: TaskService
+    private taskService: TaskService,
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router
   ) { }
 
+  editHuntForm = this.formBuilder.group({
+    title: ['', Validators.compose([
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(50)])],
+    description: ['', Validators.compose([
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(140)])],
+  });
+
+  editTaskForm: FormArray = this.formBuilder.array([]);
+
+  get tasks() {
+    return this.editTaskForm.get('tasks') as FormArray;
+  }
+
+  createTaskFormGroup(): FormGroup {
+    return this.formBuilder.group({
+      taskInput: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(140)]]
+    });
+  }
+
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.huntService.getHunt(id).subscribe(hunt => {
-      this.hunt = hunt;
-      this.taskService.getTasksForHunt(hunt._id).subscribe(tasks => {
-        this.tasks = tasks;
-      });
+    this.tasks.controls.forEach((taskControl: FormGroup) => {
+      const task = taskControl.value as Task;
+      this.editTaskForm.push(this.formBuilder.group({
+        taskInput: [task.description, [Validators.required, Validators.minLength(2), Validators.maxLength(140)]]
+      }));
     });
   }
 
   onSubmit(): void {
-    this.huntService.updateHunt(this.hunt._id, this.hunt).subscribe();
-    this.tasks.forEach(task => {
-      this.taskService.updateTask(task._id, task).subscribe();
+    this.huntService.updateHunt(this.hunt._id, this.editHuntForm.value).subscribe();
+    this.tasks.controls.forEach((taskControl, index) => {
+      this.taskService.updateTask(this.tasks[index]._id, { description: taskControl.value.taskInput }).subscribe();
     });
   }
+
+  addTaskInput() {
+    this.tasks.push(this.createTaskFormGroup());
+  }
+
+  deleteTaskInput(index: number) {
+    this.tasks.removeAt(index);
+    this.taskService.deleteTask(this.tasks[index]._id).subscribe();
+  }
+
 }
