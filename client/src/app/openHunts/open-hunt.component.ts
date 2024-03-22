d0869794f837e3d41f7422807d38252bae1a7fc0
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-03-22 12:23:13 -0500

    created @Component for open-hunt.component.ts

---
+++ client/src/app/openHunts/open-hunt.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-open-hunt',
+  standalone: true,
+  imports: [
+
+  ],
+  templateUrl: './open-hunt.component.html',
+  styleUrl: './open-hunt.component.scss'
+})
+export class OpenHuntComponent {
+
+}
