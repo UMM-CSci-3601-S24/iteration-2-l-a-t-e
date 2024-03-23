f872370f61cdd48a1041ea17e37b40fa40edfd49
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-03-23 14:58:48 -0500

    made openHunt.services.ts file

---
+++ client/src/app/open-hunts/openHunt.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: `root`
+})
+export class OpenHuntService {
+  // URL for openHunts part of server API
+  readonly huntUrl
+
+}
