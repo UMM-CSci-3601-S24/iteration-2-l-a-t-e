07660d228678705851820c6bb25b514dacb6d0c0
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-03-23 14:58:09 -0500

    made test for getting wheter an openHunt is active or not

--- client/src/app/open-hunts/openHunt.service.spec.ts
+++ client/src/app/open-hunts/openHunt.service.spec.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from "@angular/common/http";
 import { OpenHunt } from "./openHunt"
 import { OpenHuntService } from "./openHunt.service";
+import { HttpTestingController } from "@angular/common/http/testing";
 
 
 describe('OpenHuntSevice', () => {
@@ -16,24 +18,53 @@ describe('OpenHuntSevice', () => {
       invitecode: 'hunt2',
       active: true,
       numberofgroups: 2,
-      groupids: ["group1","group2"]
+      groupids: ["group1", "group2"]
     },
     {
       _id: 'hunt3_id',
       invitecode: 'hunt3',
       active: true,
       numberofgroups: 3,
-      groupids: ["group1","group2","group3"]
+      groupids: ["group1", "group2", "group3"]
     },
     {
       _id: 'hunt4_id',
       invitecode: 'hunt4',
       active: false,
       numberofgroups: 4,
-      groupids: ["group1","group2","group3","group4"]
+      groupids: ["group1", "group2", "group3", "group4"]
     }
   ];
 
   let openHuntService: OpenHuntService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
-})
+  beforeEach(() => {
+    // Set up the mock handling of the HTTP requests
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    huntService = TestBed.inject(HuntService);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
+
+  describe('test', () => {
+    it('should get active hunts', () => {
+      openHuntService.getActive().subscribe(hunts => {
+        expect(hunts.length).toBe(4);
+        expect(hunts).toEqual(testOpenHunts);
+
+        const req = httpTestingController.expectOne('api/openHunt/active');
+        expect(req.request.method).toEqual('GET');
+      });
+    })
+  });
+});
