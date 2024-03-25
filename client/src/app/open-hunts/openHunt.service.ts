0e2aa9cbc47952cab421c85ea10a0a7743a4eaad
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-03-25 12:51:05 -0500

    using code from Hunt.service in openHunt.service (both spec.ts and ts)

--- client/src/app/open-hunts/openHunt.service.ts
+++ client/src/app/open-hunts/openHunt.service.ts
@@ -1,10 +1,57 @@
 import { Injectable } from "@angular/core";
+import { environment } from "../../environments/environment";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { OpenHunt } from "./openHunt";
+import { Observable, Subject, map, tap } from "rxjs";
 
 @Injectable({
   providedIn: `root`
 })
 export class OpenHuntService {
   // URL for openHunts part of server API
-  readonly huntUrl
+  readonly openHuntUrl: string = `${environment.apiUrl}open-hunts`
+
+  private readonly hostKey = 'hostid';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getOpenHunts(filters?: { hostid?: string }): Observable<OpenHunt[]> {
+    let httpParams: HttpParams = new HttpParams();
+    if (filters) {
+      if (filters.hostid) {
+        httpParams = httpParams.set(this.hostKey, filters.hostid);
+      }
+    }
+
+    return this.httpClient.get<OpenHunt[]>(this.openHuntUrl, {
+      params: httpParams
+    });
+  }
+
+  getOpenHuntById(id: string): Observable<OpenHunt> {
+    // The input to get could also be written as (this.openHuntUrl + '/' + id)
+    return this.httpClient.get<OpenHunt>(`${this.openHuntUrl}/${id}`);
+  }
+
+  addOpenHunt(newHunt: Partial<OpenHunt>): Observable<string> {
+    // Send post request to add a new hunt with the user data as the body.
+    return this.httpClient.post<{ id: string }>(this.openHuntUrl, newHunt).pipe(map(res => res.id));
+  }
+
+  openHuntDeleted = new Subject<void>();
+
+  deleteOpenHunt(huntId: string): Observable<void> {
+    return this.httpClient.delete<void>(`${this.openHuntUrl}/${huntId}`).pipe(
+      tap(() => {
+        // Emit the huntDeleted event
+        this.openHuntDeleted.next();
+      })
+    );
+  }
+
+
+
+
 
 }
