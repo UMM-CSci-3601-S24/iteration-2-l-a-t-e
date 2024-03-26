a3ae4fb98a66d4afc7c8777d0dc897e81d69f5ae
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-03-26 13:23:31 -0500

    changed openHunt framework structure

--- client/src/app/open-hunts/openHunt.ts
+++ client/src/app/open-hunts/openHunt.ts
@@ -1,9 +1,16 @@
 
 export interface OpenHunt {
   _id: string;
-  invitecode: string;
+
   active: boolean;
+  hostid: string;
+  huntid: string;
+  title: string;
+  description: string;
+  invitecode: string;
   numberofgroups: number;
   groupids: Array<string>
+  groups: Array<Group>
+
   // Todo: Possibly put a reference here to the actual hunt it is an instance of
 }
