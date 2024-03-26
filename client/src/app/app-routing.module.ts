98e6e07541f7605eb3c5b82006d0c8eba3f23206
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-03-26 13:22:28 -0500

    comment for clarification

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
   {path: 'tasks', component: TaskListComponent, title: 'Task List'},
   {path: 'tasks/new', component: TaskListComponent, title: 'New Task'},
   {path: 'tasks/:id', component: TaskListComponent, title: 'Single Task'},
-  {path: 'open-hunts/:id', component: OpenHuntComponent, title: 'Hunt'}
+  {path: 'open-hunts/:id', component: OpenHuntComponent, title: 'Hunt'} // id for coresponing hunt, not openhunt id.
 
 ];
 
