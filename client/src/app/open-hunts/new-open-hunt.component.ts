3161f82195f3277ff3e6bbbdbd1a243e87cbfbbe
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-03-27 12:00:28 -0500

    created new-open-hunt.component

---
+++ client/src/app/open-hunts/new-open-hunt.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Subject } from 'rxjs';
+import { map, switchMap, takeUntil } from 'rxjs/operators';
+import { Hunt } from '../hunts/hunt';
+import { Task } from '../hunts/task';
+import { HuntService } from '../hunts/hunt.service';
+import { TaskService } from '../hunts/task.service';
+
+
+
+@Component({
+  selector: 'app-new-open-hunt',
+  standalone: true,
+  imports: [
+    MatCardModule
+  ],
+  templateUrl: './new-open-hunt.component.html',
+  styleUrl: './new-open-hunt.component.scss'
+})
+export class NewOpenHuntComponent implements OnInit, OnDestroy{
+  hunt: Hunt;
+  tasks: Task[] = [];
+  error: { help: string, httpResponse: string, message: string };
+
+  private ngUnsubscribe = new Subject<void>();
+
+  constructor(private snackBar: MatSnackBar, private route: ActivatedRoute, private huntService: HuntService, private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.pipe(
+      map((paramMap: ParamMap) => paramMap.get('id')),
+      switchMap((id: string) => this.huntService.getHuntById(id)),
+      switchMap((hunt: Hunt) => {
+        this.hunt = hunt;
+        return this.taskService.getTasks({ huntid: hunt._id });
+      }),
+      takeUntil(this.ngUnsubscribe)
+    ).subscribe({
+      next: (tasks: Task[]) => {
+        this.tasks = tasks;
+      },
+      error: _err => {
+        this.error = {
+          help: 'There was a problem loading the hunt - try again.',
+          httpResponse: _err.message,
+          message: _err.error?.title,
+        };
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+
+    this.ngUnsubscribe.complete();
+  }
+}
